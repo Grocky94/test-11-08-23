0bbe3328066a7ef63d1c3e8bfb87a55adb677a74
Grocky94 <123794431+Grocky94@users.noreply.github.com>
2023-08-11 17:07:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+// Q . 1
+// let nums = [3,3]
+// let target = 6
+
+
+// function sum(nums, target) {
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         for (let j = 1; j < nums.length; j++) {
+//            if(nums[i] + nums[j] === target){
+//             console.log([i , j])
+//            }
+
+//         }
+
+//     }
+//     // let sort = nums.sort((a,b) = (a+b))
+//     // console.log(sort)
+// }
+// sum(nums,target)
+
+
+// Q.2 
+
+nums = [1, 1, 2,2,3,3,3,4,4]
+
+function similar(nums) {
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     for (let j = 1; j < nums.length; j++) {
+    //         if (nums[i] !== nums[j]) {
+    //             nums.splice(index, [i]==[j])
+    //             console.log(nums)
+    //         }
+    //     }
+    // }
+    
+}
+similar(nums)
